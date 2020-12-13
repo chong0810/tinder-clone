994bf9c11a072ee381261607c94fbbd1c8c66c2e
Daniel Hong <hongchangyeon@gmail.com>
2020-12-13 13:08:03 -0600

    commit before forking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Let's build the MERN TInder clone guys!!!</h1>
+
+
+      {/* {Header} */}
+      {/* {TinderCards} */}
+      {/* {SwipeButtons} */}
     </div>
   );
 }
